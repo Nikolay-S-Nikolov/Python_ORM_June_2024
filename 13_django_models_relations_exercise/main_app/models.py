ede1865cca595954058a99fdb51ffbf2fa09aa6a
Nikolay Nikolov <nikolay.s.nikolov@gmail.com>
2024-07-16 11:26:19 +0300

    Added more solutions

---
+++ 13_django_models_relations_exercise/main_app/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+
+
+# 01. Library ----------------------------------------------------------------
+
+class Author(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=40)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+
+
+# 02. Music App ----------------------------------------------------------------
+
+class Song(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    songs = models.ManyToManyField('Song', related_name='artists')
+
+
+# 03. Shop ----------------------------------------------------------------
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Review(models.Model):
+    description = models.TextField(max_length=200)
+    rating = models.PositiveSmallIntegerField()
+    product = models.ForeignKey(
+        'Product',
+        on_delete=models.CASCADE,
+        related_name='reviews')
+
+
+# 04. License ----------------------------------------------------------------
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+class DrivingLicense(models.Model):
+    license_number = models.CharField(max_length=10, unique=True)
+    issue_date = models.DateField()
+    driver = models.OneToOneField("Driver", on_delete=models.CASCADE, related_name='license')
+
+
+# 05. Car Registration ----------------------------------------------------------------
+
+class Owner(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    owner = models.ForeignKey(
+        'Owner',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='cars',
+    )
+
+
+class Registration(models.Model):
+    registration_number = models.CharField(max_length=10, unique=True)
+    registration_date = models.DateField(null=True, blank=True)
+    car = models.OneToOneField(
+        'Car',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='registration',
+    )
