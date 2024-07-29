fd126fbe504984f670b6b7a62013a72181ced627
Nikolay Nikolov <nikolay.s.nikolov@gmail.com>
2024-07-29 16:58:16 +0300

    Added more solutions

---
+++ 23_exam_preparation/36_python_orm_regular_exam_26_november_2023_new/main_app/custom_model_managers.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.db.models import Count
+
+
+class AuthorManager(models.Manager):
+    def get_authors_by_article_count(self):
+        return self.annotate(num_article=Count('articles')).order_by('-num_article', 'email')
